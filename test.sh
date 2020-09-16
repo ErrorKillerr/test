595c1ddc24c90173950498c9d65612820b641f5c
ErrorKillerr <70461560+ErrorKillerr@users.noreply.github.com>
2020-09-16 15:18:53 +0530

    Add files via upload

---
+++ test.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+red='\033[1;31m'
+rset='\033[0m'
+grn='\033[1;32m'
+
+echo -e $red"MY 1ST SCRIPT"
+clear
+echo -e $grn"press enter continu"
+read continu
+echo -e "update found "
+echo -e "press ( Y ) to update press ( N ) exit"
+echo
+read -p'input:--' enter;
+
+if [ "${enter}" = "Y" ]
+then
+git pull
+clear
+figlet -f slant "U P D A T E D"|lolcat
+sleep 1
+fi
+
+if [ "${enter}" = "N" ]
+then
+figlet -f slant "W A I T"|lolcat
+sleep 1
+rm -rf 
+exit
+fi
+
